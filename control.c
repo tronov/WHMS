28e2ddc70f003e8df810cd356eaf9e5ccf1ea66c
MalkavianMax <malkavianmax@gmail.com>
2016-01-25 06:25:00 +0300

    Final functions basic implementation

--- control.c
+++ control.c
@@ -31,7 +31,7 @@ unsigned char control_state, control_state_prev;
 #define CONTROL_PWM_MIN         0
 #define CONTROL_PWM_MAX         255
 // Минимальный модуль тангенса угла наклона касательной вольт-амперной характеристики (порог чувствительности)
-#define CONTROL_I_TG_ABS_MIN    1.0
+#define CONTROL_I_TG_ABS_MIN    0.01
 // Timeout in ticks (1 tick ~ 0.1 millisecond)
 #define CONTROL_TIMEOUT         (1 * SEC)   // Тайм-аут запуска измерений
 #define CONTROL_TIMEOUT_R_ON    (100 * MS)  // Тайм-аут переходных процессов после подачи напряжения на реле
@@ -44,14 +44,14 @@ unsigned char control_state, control_state_prev;
 // Объявление приватных переменных
 struct Point2
 {
-    double X;
-    double Y;
+    volatile double X;
+    volatile double Y;
 };
 struct Point3
 {
-    double X;
-    double Y;
-    double Z;
+    volatile double X;
+    volatile double Y;
+    volatile double Z;
 };
 unsigned int control_pwm;
 volatile double control_g,
@@ -61,18 +61,43 @@ volatile double control_g,
 volatile struct Point3 control_report_data [CONTROL_RUSHES_MAX];
 volatile unsigned char control_report_data_i = 0;
 
-
 // Объявление приватных функций
 void send_report(void);
 struct Point3 get_derivative(struct Point2 p);
 void make_calculations(void);
 
-// Указатель для работы с параметром - массивом команды
-unsigned char *cmd_ptr;
 
 // Массив отчета и его индекс
 unsigned char report[REP_SIZE];
 volatile unsigned char report_i;
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// get_derivative
+volatile unsigned char iter = 0;
+volatile double x1 = 0, x2 = 0, x3 = 0, x4 = 0, x5 = 0, h = 0;
+volatile double   y_1 = 0,   y2 = 0,   y3 = 0,   y4 = 0,   y5 = 0,
+                d1y1 = 0, d1y2 = 0, d1y3 = 0, d1y4 = 0,
+                d2y1 = 0, d2y2 = 0, d2y3 = 0,
+                d3y1 = 0, d3y2 = 0,
+                d4y1 = 0;
+volatile double y_derivative = 0;
+volatile struct Point3 result;
+
+// make_calculations
+volatile struct Point3 control_point3_prev = {0.0, 0.0, 0.0};
+volatile unsigned char is_in_rush = 0;  // Флаг участка броска тока
+volatile struct Point2 new_point, local_max = {0.0, 0.0};
+volatile struct Point3 current;
+volatile double i_tan;
+
+//////////////////////////////////////////////////////////////////////////
+
+
+
 
 
 void control_init(void)
@@ -205,7 +230,7 @@ void control_proc(void)
             send_message_w_param(MSG_PWM_SET, &control_pwm);
             break;
         case CONTROL_STATE_REPORT:
-            //send_report();
+            send_report();
             break;
         default: break;
         }
@@ -232,8 +257,18 @@ void send_report()
     report[report_i++] = REP_DELIM;
     
     volatile unsigned char i;    
-    for (i = 0; i < control_report_data_i; i++)
+    for (i = 0; i < control_report_data_i; i++) // control_report_data_iCONTROL_RUSHES_MAX
     {
+        dtostrf( control_report_data[i].Y, 1, 5, (char *) &buffer);
+        
+        for (buffer_i = 0; buffer[buffer_i] != REP_EOL;)
+        {
+            report[report_i++] = buffer[buffer_i++];
+        }
+        
+        report[report_i++] = REP_DELIM;
+        
+        
         volatile double delta_i = control_report_data[i].X - control_report_data[i].Z;        
         dtostrf( delta_i, 1, 5, (char *) &buffer);
         
@@ -244,6 +279,8 @@ void send_report()
         
         report[report_i++] = REP_DELIM;
     }
+    
+    control_report_data_i = 0;    // Сброс индекса для разблокировки приращения частоты ШИМ
 
     report[report_i]   = REP_LF;  // Подмена последнего символа REP_DELIM
     report[report_i++] = REP_CR;
@@ -255,19 +292,13 @@ void send_report()
 
 struct Point3 get_derivative(struct Point2 p)
 {
-    volatile static int iter = 0;
-    
-    volatile static double x1 = 0, x2 = 0, x3 = 0, x4 = 0, x5 = 0, h = 0;
-    
-    volatile static double y_derivative = 0;
-    
-    volatile static double   y1 = 0,   y2 = 0,   y3 = 0,   y4 = 0,   y5 = 0,
-                           d1y1 = 0, d1y2 = 0, d1y3 = 0, d1y4 = 0,
-                           d2y1 = 0, d2y2 = 0, d2y3 = 0,
-                           d3y1 = 0, d3y2 = 0,
-                           d4y1 = 0;
-    
-    volatile struct Point3 result = {x1, y1, y_derivative};
+    //result.X = x1;
+    //result.Y = y_1;
+    //result.Z = y_derivative;
+
+    result.X = 0.0;
+    result.Y = 0.0;
+    result.Z = 0.0;
 
     if (x5 - p.X == 0) return result;
     
@@ -276,16 +307,12 @@ struct Point3 get_derivative(struct Point2 p)
     x3 = x4;
     x4 = x5;
     x5 = p.X;
-    
-    result.X = x1;
 
-    y1 = y2;
+    y_1 = y2;
     y2 = y3;
     y3 = y4;
     y4 = y5;
     y5 = p.Y;
-    
-    result.Y = y1;
     
     d1y1 = d1y2;
     d1y2 = d1y3;
@@ -301,50 +328,76 @@ struct Point3 get_derivative(struct Point2 p)
     
     d4y1 = d3y2 - d3y1;
     
-    if (iter++ < 4) return result;
+    // Функция возвращает точку с начала буфера
+    // Восемь итераций на заполнение буфера точек и дельт
+    if (iter++ < 8) return result;
     
     h = ((x5 - x4) + (x4 - x3) + (x3 - x2) + (x2 - x1)) / 4;
     
-    y_derivative = (1 / (h * h)) * (d1y1 - (d2y1 / 2) + (d3y1 / 3) - (d4y1/4));
+    y_derivative = (1 / (h * h)) * (d1y1 - (d2y1 / 2) + (d3y1 / 3) - (d4y1 / 4));
 
+    result.X = x1;
+    result.Y = y_1;
     result.Z = y_derivative;
+    
     return result;
 }
 
+
+
+
+
 void make_calculations()
 {
-    volatile static struct Point3 control_point3_prev = {0.0, 0.0, 0.0};
-    volatile static unsigned char is_in_rush = 0;  // Флаг участка броска тока
+    new_point.X = control_u;
+    new_point.Y = control_i;
     
-    volatile struct Point2 new_point = { control_u, control_i };
-    
-    volatile struct Point3 current = get_derivative(new_point);
+    current = get_derivative(new_point);
     
     if (current.X == 0 && current.Y == 0 && current.Z == 0) return;
-    if (control_point3_prev.X == 0 && control_point3_prev.Y == 0 && control_point3_prev.Z == 0) return;
+    
+    if (control_point3_prev.X == 0 && control_point3_prev.Y == 0 && control_point3_prev.Z == 0)
+    {
+        control_point3_prev = current;
+        return;
+    }        
     if (current.X - control_point3_prev.X == 0) return;
     
-    volatile double tan = (current.Y - control_point3_prev.Y) / (current.X - control_point3_prev.X);
+    i_tan = (current.Y - control_point3_prev.Y) / (current.X - control_point3_prev.X);
     
     
-    if (fabs(tan) > CONTROL_I_TG_ABS_MIN)
+    if (fabs(i_tan) > CONTROL_I_TG_ABS_MIN)
+    // Если текущая точка находится на участке броска тока
     {
         if (!is_in_rush)
+        // Если текущая точка первая на участке броска тока
         {
             is_in_rush = 1;
             control_report_data[control_report_data_i].X = current.Y;
         }
         else
         {
-            if (control_report_data[control_report_data_i].Y < current.Y)
-                control_report_data[control_report_data_i].Y = current.Y;
+            if (local_max.Y < current.Z)
+            {
+                local_max.Y = current.Z;
+                local_max.X = current.X;
+            }                
         }
     }
     else
+    // Если текущая точка не находится на участке броска тока
     {
-        is_in_rush = 0;
-        control_report_data[control_report_data_i].Z = current.Y;
-        control_report_data_i++;
+        if (is_in_rush)
+        // Если текущая точка первая после участка броска тока
+        {
+            control_report_data[control_report_data_i].Y = local_max.X;            
+            control_report_data[control_report_data_i].Z = current.Y;
+            control_report_data_i++;
+            
+            is_in_rush = 0;
+            local_max.X = 0.0;
+            local_max.Y = 0.0;
+        }
     }
      
     control_point3_prev = current;
